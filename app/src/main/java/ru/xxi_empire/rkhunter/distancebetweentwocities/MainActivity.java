2f2405ebdd3f8b921631cf365f81dca969b2f692
Vlad <rkhunter@xxi-empire.ru>
2016-12-20 12:10:51 -0800

    Stage 1

--- app/src/main/java/ru/xxi_empire/rkhunter/distancebetweentwocities/MainActivity.java
+++ app/src/main/java/ru/xxi_empire/rkhunter/distancebetweentwocities/MainActivity.java
@@ -1,22 +1,51 @@
 package ru.xxi_empire.rkhunter.distancebetweentwocities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 import ru.xxi_empire.rkhunter.distancebetweentwocities.helpers.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
+    InputMethodManager inputManager;
+
     AutoCompleteTextView autocompleteInputCity1;
     AutoCompleteTextView autocompleteInputCity2;
+    Button calculateButton;
 
+    private void checkInputGroup() {
+        if (
+                !autocompleteInputCity1.getText().toString().matches("")
+                &&
+                !autocompleteInputCity2.getText().toString().matches("")
+        ) {
+            this.calculateButton.setEnabled(true);
+        } else {
+            this.calculateButton.setEnabled(false);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intent = new Intent(getBaseContext(), MapsActivity.class);
+        inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        calculateButton = (Button) findViewById(R.id.button_calculate);
+        // calculateButton.setEnabled(false);
+
 
         autocompleteInputCity1 = (AutoCompleteTextView) findViewById(R.id.input_city_1);
         autocompleteInputCity1.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.autocomplete_list_item));
@@ -27,7 +56,27 @@ public class MainActivity extends AppCompatActivity {
                 // Get data associated with the specified position
                 // in the list (AdapterView)
                 String description = (String) parent.getItemAtPosition(position);
-                Toast.makeText(getApplicationContext(), description, Toast.LENGTH_SHORT).show();
+                intent.putExtra("HOME_CITY", description);
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+                checkInputGroup();
+            }
+        });
+
+        autocompleteInputCity1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                checkInputGroup();
             }
         });
 
@@ -40,7 +89,33 @@ public class MainActivity extends AppCompatActivity {
                 // Get data associated with the specified position
                 // in the list (AdapterView)
                 String description = (String) parent.getItemAtPosition(position);
-                Toast.makeText(getApplicationContext(), description, Toast.LENGTH_SHORT).show();
+                intent.putExtra("DESTINATION_CITY", description);
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+        });
+
+        autocompleteInputCity2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                checkInputGroup();
+            }
+        });
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/ru/xxi_empire/rkhunter/distancebetweentwocities/PlacesAutoCompleteAdapter.java
+++ app/src/main/java/ru/xxi_empire/rkhunter/distancebetweentwocities/PlacesAutoCompleteAdapter.java
@@ -1,9 +1,14 @@
 package ru.xxi_empire.rkhunter.distancebetweentwocities;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Filterable;
 import android.widget.Filter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -45,9 +50,12 @@ public class PlacesAutoCompleteAdapter extends ArrayAdapter<String> implements F
         Filter filter = new Filter() {
             @Override
             protected FilterResults performFiltering(CharSequence constraint) {
-                FilterResults filterResults = new Filter.FilterResults();
+                FilterResults filterResults = new FilterResults();
                 if (constraint != null) {
                     resultList = mPlaceAPI.autocomplete(constraint.toString());
+
+                    // Footer
+                    resultList.add("footer");
 
                     filterResults.values = resultList;
                     filterResults.count = resultList.size();
@@ -67,5 +75,29 @@ public class PlacesAutoCompleteAdapter extends ArrayAdapter<String> implements F
         };
 
         return filter;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view;
+
+        //if (convertView == null) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (position != (resultList.size() - 1))
+            view = inflater.inflate(R.layout.autocomplete_list_item, null);
+        else
+            view = inflater.inflate(R.layout.autocomplete_google_logo, null);
+        //}
+        //else {
+        //    view = convertView;
+        //}
+
+        if (position != (resultList.size() - 1)) {
+            TextView autocompleteTextView = (TextView) view.findViewById(R.id.autocompleteText);
+            autocompleteTextView.setText(resultList.get(position));
+        }
+
+        return view;
     }
 }
